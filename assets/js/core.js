efa57a174092eb061c154157f1ed3d3e928c9f0d
rajmohanpdy <raj4057@gmail.com>
2021-05-16 09:31:39 +0530

    first commit

---
+++ assets/js/core.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.testimonial-slider', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 00
+      }
+    }
+});
